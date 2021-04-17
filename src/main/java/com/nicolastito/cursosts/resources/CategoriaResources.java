cabb684200ebde5faf3b9510153c4f8c49386adb
nicolastitto <mrnicolastito.tito@gmail.com>
2021-04-17 16:50:23 +0100

    create a categoria

--- src/main/java/com/nicolastito/cursosts/resources/CategoriaResources.java
+++ src/main/java/com/nicolastito/cursosts/resources/CategoriaResources.java
@@ -1,5 +1,10 @@
 package com.nicolastito.cursosts.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.nicolastito.cursosts.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
   
   @RequestMapping (method = RequestMethod.GET)
-  public String listar(){
-    return "Teste";
+  public List <Categoria> listar(){
+    
+    Categoria cat1 = new Categoria(1, "Informática");
+    Categoria cat2 = new Categoria(2, "Escritório");
+
+    List<Categoria> lista = new ArrayList<>();
+    lista.add (cat1);
+    lista.add (cat2);
+
+    return lista;
   }
-  
 }
