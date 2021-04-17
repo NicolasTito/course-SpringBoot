8663e15b49cdc6083fa3018f85a1d8fb847af53b
nicolastitto <mrnicolastito.tito@gmail.com>
2021-04-17 21:57:19 +0100

    endpoint adjustments

--- src/main/java/com/nicolastito/cursosts/services/CategoriaService.java
+++ src/main/java/com/nicolastito/cursosts/services/CategoriaService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.nicolastito.cursosts.domain.Categoria;
 import com.nicolastito.cursosts.repositories.CategoriaRepository;
+import com.nicolastito.cursosts.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +18,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+			"Object not found! Id: " + id + ", Type: " + Categoria.class.getName(), null));
 	}
 }
