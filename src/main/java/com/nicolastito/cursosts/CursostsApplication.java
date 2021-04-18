6d6eab6615701f8595fa576331e3a22975da9480
nicolastitto <mrnicolastito.tito@gmail.com>
2021-04-18 08:03:48 +0100

    create states and city

--- src/main/java/com/nicolastito/cursosts/CursostsApplication.java
+++ src/main/java/com/nicolastito/cursosts/CursostsApplication.java
@@ -3,9 +3,13 @@ package com.nicolastito.cursosts;
 import java.util.Arrays;
 
 import com.nicolastito.cursosts.domain.Categoria;
+import com.nicolastito.cursosts.domain.City;
 import com.nicolastito.cursosts.domain.Product;
+import com.nicolastito.cursosts.domain.State;
 import com.nicolastito.cursosts.repositories.CategoriaRepository;
+import com.nicolastito.cursosts.repositories.CityRepository;
 import com.nicolastito.cursosts.repositories.ProductRepository;
+import com.nicolastito.cursosts.repositories.StateRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,10 @@ public class CursostsApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private StateRepository stateRepository;
+	@Autowired
+	private CityRepository cityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursostsApplication.class, args);
@@ -41,8 +49,22 @@ public class CursostsApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+		State est1 = new State(null, "Minas Gerais");
+		State est2 = new State(null, "São Paulo");
+
+		City c1 = new City(null, "Umberlandia", est1);
+		City c2 = new City(null, "São Paulo", est2);
+		City c3 = new City(null, "Campinas", est2);
+
+		est1.getCities().addAll(Arrays.asList(c1));
+		est2.getCities().addAll(Arrays.asList(c2, c3));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		stateRepository.saveAll(Arrays.asList(est1, est2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
---
+++ src/main/java/com/nicolastito/cursosts/domain/enums/TypeClient.java
@@ -0,0 +1,5 @@
+package com.nicolastito.cursosts.domain.enums;
+
+public enum TypeClient {
+	
+}
